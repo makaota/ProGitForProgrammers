5201d4cdd272b32ce480cad57b3e22e07fcac2b8
Makaota <74915165+makaota@users.noreply.github.com>
2022-01-09 19:35:35 +0200

    Visual Studio programmers added a book class

---
+++ ProGitForProgrammers/ProGitForProgrammers/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProGitForProgrammers
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public List<string> Authors { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+    }
+}
