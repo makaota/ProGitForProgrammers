d73e18d01baeb40026e680a8df8b5a69f69bc975
Makaota <74915165+makaota@users.noreply.github.com>
2022-01-09 16:15:54 +0200

    added staging area line in visual studio

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I Just Added this in Visual Studio");
 Console.WriteLine("I just added this in the command line repo");
+Console.WriteLine("This line is added to show the staging area");
