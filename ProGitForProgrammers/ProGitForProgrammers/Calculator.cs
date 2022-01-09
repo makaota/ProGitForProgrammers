cbd4236e74c1947146d4bce7ec8f75ce408b85be
Makaota <74915165+makaota@users.noreply.github.com>
2022-01-09 19:55:43 +0200

    Add Multiply method

--- ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
@@ -17,5 +17,15 @@ namespace ProGitForProgrammers
         {
             return left - right;
         }
+
+        public int Multiply(int left, int right)
+        {
+            return left * right;
+        }
+        public int Divide(int left, int right)
+        {
+            return left / right;
+        }
+
     }
 }
