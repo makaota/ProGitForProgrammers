4802f8334beb2d27a7e512b57938d6e8896f9e1f
Makaota <74915165+makaota@users.noreply.github.com>
2022-01-09 16:37:59 +0200

    Update Program.cs
    A line was added from the GitHub desktop

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("I Just Added this in Visual Studio");
 Console.WriteLine("I just added this in the command line repo");
 Console.WriteLine("This line is added to show the staging area");
+Console.WriteLine("This line is added to show the staging area in GutHub for descktop");
